5d1c21464b12a5ca0b55b7a17935f7064194993b
volcmen <volcmen@gmail.com>
2026-02-10 18:00:41 +0200

    🔧 update fish conf

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,12 +1,31 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    # No greeting
-    set -g fish_greeting
 
     starship init fish | source
     zoxide init fish | source
 
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+
+    # eza
     alias ls 'eza --icons'
+    alias tree 'eza --tree --icons'
+
+    alias cat 'bat --plain'
+    # alias grep rg
     alias cd z
+    alias find fd
+
+    # Shorthands
     alias nv nvim
+    alias lg lazygit
 end
+
+# No greeting
+set -g fish_greeting
